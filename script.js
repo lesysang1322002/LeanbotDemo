5cbdc1fab071216000425c9ea4412c885fb35061
lesysang1322002 <128148253+lesysang1322002@users.noreply.github.com>
2024-01-18 16:57:26 +0700

    update 18/01

--- script.js
+++ script.js
@@ -104,52 +104,72 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-let ir2L,ir0L,ir1R,ir3R,ir4L,ir6L,ir5R,ir7R,TB1A,TB1B,TB2A,TB2B,distance="";
+let ir2L,ir0L,ir1R,ir3R,ir4L,ir6L,ir5R,ir7R,TB1A,TB1B,TB2A,TB2B,distance="",i;
+const distanceContainer = document.getElementById("distanceContainer");
+// Kiểm tra giá trị distance và thay đổi nội dung tương ứng
 const slider = document.getElementById('distanceSlider');
 const distanceValue = document.getElementById('distanceValue');
+let string="";
 function handleChangedValue(event) {
     let data = event.target.value;
     let dataArray = new Uint8Array(data.buffer);
     let textDecoder = new TextDecoder('utf-8');
     let valueString = textDecoder.decode(dataArray);
-    if(valueString[0]=='T'){
-        TB1A=valueString[3];
-        TB1B=valueString[4];
-        TB2A=valueString[5];
-        TB2B=valueString[6];
-        ir6L=valueString[13];
-        ir4L=valueString[14];
-        ir2L=valueString[16];
-        ir0L=valueString[17];
-        ir1R=valueString[18];
-        ir3R=valueString[19];
-        }
-        else if(valueString[0]==' '){
-            ir5R=valueString[1];
-            ir7R=valueString[2];
-            let i = 6;
+    let n = valueString.length;
+    if(valueString[n-1]=='\n'){
+        string+=valueString;
+        console.log(string);
+        if(string[0]=='T'){
+            TB1A=string[3];
+            TB1B=string[4];
+            TB2A=string[5];
+            TB2B=string[6];
+            ir6L=string[13];
+            ir4L=string[14];
+            ir2L=string[16];
+            ir0L=string[17];
+            ir1R=string[18];
+            ir3R=string[19];
+            ir5R=string[21];
+            ir7R=string[22];
+            i=26;
             distance="";
-            while(valueString[i] !== ' ') {
-                distance += valueString[i];
+            while(string[i]!=' '){
+                distance +=string[i];
                 i++;
             }
-        }
-        console.log("TB: " + TB1A+TB1B+TB2A+TB2B);
-        console.log("IR: " + ir6L+ ir4L +" "+ ir2L+ir0L+ir1R+ir3R + " " +ir5R+ir7R);
-        updateBackground('ir2L', ir2L);
-        updateBackground('ir0L', ir0L);
-        updateBackground('ir1R', ir1R);
-        updateBackground('ir3R', ir3R);
-        updateBackground('ir4L', ir4L);
-        updateBackground('ir6L', ir6L);
-        updateBackground('ir5R', ir5R);
-        updateBackground('ir7R', ir7R);
-        updateBackground('TB1A', TB1A);
-        updateBackground('TB1B', TB1B);
-        updateBackground('TB2A', TB2A);
-        updateBackground('TB2B', TB2B);
-        slider.value = distance;
-        distanceValue.textContent = `${distance} cm`;
+            if (distance === "1000") {
+                distanceContainer.innerHTML = '<h6>HC-SR04 Ultrasonic distance</h6>';
+              } else {
+                distanceContainer.innerHTML = `<input type="range" id="distanceSlider" min="0" max="100" value="0" />`;
+              }
+            console.log("TB: " + TB1A+TB1B+TB2A+TB2B);
+            console.log("IR: " + ir6L+ ir4L +" "+ ir2L+ir0L+ir1R+ir3R + " " +ir5R+ir7R);
+            console.log("Distance: " + distance);
+            updateBackground('ir2L', ir2L);
+            updateBackground('ir0L', ir0L);
+            updateBackground('ir1R', ir1R);
+            updateBackground('ir3R', ir3R);
+            updateBackground('ir4L', ir4L);
+            updateBackground('ir6L', ir6L);
+            updateBackground('ir5R', ir5R);
+            updateBackground('ir7R', ir7R);
+            updateBackground('TB1A', TB1A);
+            updateBackground('TB1B', TB1B);
+            updateBackground('TB2A', TB2A);
+            updateBackground('TB2B', TB2B);
+            slider.value = distance;
+            distanceValue.textContent = `${distance} cm`;
+            }
+        string="";
+    }
+    else{
+        string+=valueString;
+    }
+}
+
+function log(text){
+    
 }
 function updateBackground(id, value) {
     const element = document.getElementById(id);
@@ -159,9 +179,16 @@ function updateBackground(id, value) {
         element.classList.add('white');
         console.log(`Background ${id} is white`);
     } else {
+        if(id=="TB1A" || id=="TB1B" ||id=="TB2A" || id=="TB2B" ){
+            element.classList.remove('white');
+            element.classList.add('red');
+            console.log(`Background ${id} is red`);
+        }
+        else{
         element.classList.remove('white');
         element.classList.add('black');
         console.log(`Background ${id} is black`);
+        }
     }
 }
 // let tabIndex = valueString.indexOf('\t');
